c3fbf56d4a3516c7a49c5a99761d9f5e5a4f7304
Lewis <lewisomogi01@gmail.com>
2025-02-09 01:27:14 +0300

    playing around with conditions in python

--- condition.py
+++ condition.py
@@ -1,12 +1,9 @@
 x=int(input("what is x?"))
 y=int(input("what is y?"))
 
-if x<y:
-    print("x is less than y")
-if x>y:
-    print("x is greater than y")
-
 if x==y:
     print("x is equal to y")
-
+else:
+    print("x is not equal to y")
+   
 
