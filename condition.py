2d5eef8960a40360e5d82481b42b70116f7c573d
Lewis <lewisomogi01@gmail.com>
2025-02-12 20:55:54 +0300

    playing around with conditions in python

--- condition.py
+++ condition.py
@@ -1,9 +1,17 @@
 x=int(input("what is x?"))
 y=int(input("what is y?"))
 
-if x==y:
-    print("x is equal to y")
+#however we can improve the above code 
+#logically elif x == y is not a necessary evaluation to run beause if x<y and x>y its automatically x will be equal to y
+x=int(input("what is x?"))
+y=int(input("what is y?"))
+
+if x<y:
+    print("x is less than y")
+
+elif x>y:
+    print("x is greater than y")
+
 else:
-    print("x is not equal to y")
-   
+    print("x is equal to y")
 
